e1c9ecccc966d2a9c57754ce8244b197b7d0f05a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-28 16:56:46 -0400

    set up link component within navbar component to handle anchor links

--- portfolio/src/scenes/Navbar.jsx
+++ portfolio/src/scenes/Navbar.jsx
@@ -2,6 +2,21 @@ import { useState } from "react"
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import useMediaQuery from "../hooks/useMediaQuery";
 
+const Link = ({page, selectedPage, setSelectedPage}) => {
+  const lowerCasePage = page.toLowerCase();
+
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+       hover:text-yellow transition duration-500` }
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
 
 const Navbar = ({selectedPage, setSelectedPage}) => {
 
