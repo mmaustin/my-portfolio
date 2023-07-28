22a81e74ab979fbb829970cd140cafb90e4cbee8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-28 10:29:50 -0400

    added minimum styling in index.css and added early configurations in app js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,14 @@
-
+import {useState} from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
 
 function App() {
+
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <div className="text-xl font-medium text-red">
-      what's up with this??
+    <div className="app bg-deep-blue " >
+      {/* <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} /> */}
     </div>
   );
 }
