bbf3fc21b52b8790df99390c429ec478f3da63f1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-29 12:26:33 -0400

    added use state value, use effect function and conditional jsx to check if scroll is at y value of 0

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,37 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import DotGroup from "./scenes/DotGroup";
 import useMediaQuery from './hooks/useMediaQuery';
 import Navbar from './scenes/Navbar';
 
 function App() {
 
   const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPAge, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true);
+      if(window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return ()=> window.removeEventListener('scroll', handleScroll);
+  }, [])
 
   return (
   //   <h1 className="text-3xl font-bold underline">
   //   Hello world!
   // </h1>
     <div className="app bg-deep-blue " >
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPAge={isTopOfPAge} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className='w-5/6 mx-auto md:h-full '>
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- portfolio/src/scenes/Navbar.jsx
+++ portfolio/src/scenes/Navbar.jsx
@@ -18,13 +18,14 @@ const Link = ({page, selectedPage, setSelectedPage}) => {
 }
 
 
-const Navbar = ({selectedPage, setSelectedPage}) => {
+const Navbar = ({isTopOfPAge, selectedPage, setSelectedPage}) => {
 
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPAge ? "" : "bg-red";
 
   return (
-    <nav className={`z-40 w-full fixed top-0 py-6`}>
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">MA</h4>
 
