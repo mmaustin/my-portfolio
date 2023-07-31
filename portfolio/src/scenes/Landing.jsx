793c372785b63da8eadd1d96463ed9d6ade0c50d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-31 09:13:16 -0400

    added my pic to the landing page

--- portfolio/src/scenes/Landing.jsx
+++ portfolio/src/scenes/Landing.jsx
@@ -13,8 +13,16 @@ const Landing = ({setSelectedPage}) => {
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
 
-      <div>
-        
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt:16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div>
+            <img alt="profile" src="assets/my-profile.jpeg" />
+          </div>
+        ) : (
+          <div>
+            
+          </div>
+        )}
       </div>
     </section>
   )
