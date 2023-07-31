8b183357c7f33b3dd0812a3c28a385b5e995a971
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-31 09:57:25 -0400

    resized my image pic when on small screens

--- portfolio/src/scenes/Landing.jsx
+++ portfolio/src/scenes/Landing.jsx
@@ -15,13 +15,20 @@ const Landing = ({setSelectedPage}) => {
 
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt:16 md:mt-32">
         {isAboveMediumScreens ? (
-          <div>
-            <img alt="profile" src="assets/my-profile.jpeg" />
+          <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
+            before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
+            before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+              max-w-[400px] md:max-w-[600px] rounded-t-full"
+              alt="profile" src="assets/my-profile.jpeg" />
           </div>
         ) : (
-          <div>
-            
-          </div>
+          <div className="mt-14">
+            <img className="object-scale-down hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+            max-w-[400px] md:max-w-[400px] rounded-t-full"
+            alt="profile" src="assets/my-profile.jpeg" /> 
+          </div>         
         )}
       </div>
     </section>
