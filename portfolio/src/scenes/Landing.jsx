26ee9501806cfda465a0173295b32ab44f50a534
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-07-31 10:43:29 -0400

    used motion div to add delayed appearance of name and brush stroke image

--- portfolio/src/scenes/Landing.jsx
+++ portfolio/src/scenes/Landing.jsx
@@ -13,7 +13,7 @@ const Landing = ({setSelectedPage}) => {
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
 
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt:16 md:mt-32">
+      <div className="md:order-2 flex justify-center basis-3/5   z-10 mt:16 md:mt-32">
         {isAboveMediumScreens ? (
           <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
             before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
@@ -31,6 +31,32 @@ const Landing = ({setSelectedPage}) => {
           </div>         
         )}
       </div>
+        <div className="z-30 basis-2/5 mt-12 md:mt-32">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{duration: 0.5}}
+            variants={{
+              hidden: {opacity: 0, x: -50},
+              visible: { opacity: 1, x: 0}
+            }}
+          >
+            <p className="text-6xl font-playlist z-10 text-center md:text-start">
+              McCray {""}
+              <span
+                className="xs:relative xs:text-deep-blue xs:font-semibold z-20
+                  xs:before:content-brush before:absolute before:-left-[25px]
+                  before:-top-[70px] before:z-[-1]"
+              >
+                Austin
+              </span>
+            </p>
+            <p className="mt-10 mb-7 text-sm text-center md:text-start">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam dolore consequuntur sint beatae ipsum? Sint optio ut aut doloribus corporis. Beatae ab tenetur consectetur optio laudantium nulla eius magni quos!
+            </p>
+          </motion.div>
+        </div>
     </section>
   )
 }
