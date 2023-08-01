21e140487620add0fcd814a80b8373aa748e2340
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-01 10:38:59 -0400

    completed skills section with lorem temp text

--- portfolio/src/scenes/MySkills.jsx
+++ portfolio/src/scenes/MySkills.jsx
@@ -32,19 +32,93 @@ const MySkills = () => {
 
         <div className="mt-16 md:mt-0">
           {isAboveMediumScreeens ? (
-            <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
-            before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
-          >
-            <img className="z-10"
-              alt="skills" src="assets/skills-image.png" />
-          </div>
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
           ) : (
-            <img className="z-10"
-              alt="skills" src="assets/skills-image.png" />
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
           )}
         </div>
-
       </div>
+
+        <div className="md:flex md:justify-between mt-16 gap-32">
+          <motion.div
+            className="md:w-1/3 mt-10"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{duration: 0.5}}
+            variants={{
+              hidden: {opacity: 0, y: 50},
+              visible: { opacity: 1, y: 0}
+            }}          
+          >
+            <div className="relative h-32">
+              <div className="z-10">
+                <p className="font-playfair font-semibold text-5xl">01</p>
+                <p className="font-playfair font-semibold text-3xl mt-3">Experience</p>
+              </div>
+              <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+            </div>
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore reprehenderit magni vitae sunt pariatur dicta libero consequuntur voluptatum dolorem, deserunt ipsum minima illo, iusto odio! Mollitia debitis repellendus illo nisi.
+            </p>
+          </motion.div>
+
+          <motion.div
+            className="md:w-1/3 mt-10"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{delay: 0.2, duration: 0.5}}
+            variants={{
+              hidden: {opacity: 0, y: 50},
+              visible: { opacity: 1, y: 0}
+            }}          
+          >
+            <div className="relative h-32">
+              <div className="z-10">
+                <p className="font-playfair font-semibold text-5xl">02</p>
+                <p className="font-playfair font-semibold text-3xl mt-3">Innovative</p>
+              </div>
+              <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+            </div>
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore reprehenderit magni vitae sunt pariatur dicta libero consequuntur voluptatum dolorem, deserunt ipsum minima illo, iusto odio! Mollitia debitis repellendus illo nisi.
+            </p>
+          </motion.div>
+
+          <motion.div
+            className="md:w-1/3 mt-10"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount:0.5}}
+            transition={{delay: 0.4, duration: 0.5}}
+            variants={{
+              hidden: {opacity: 0, y: 50},
+              visible: { opacity: 1, y: 0}
+            }}          
+          >
+            <div className="relative h-32">
+              <div className="z-10">
+                <p className="font-playfair font-semibold text-5xl">03</p>
+                <p className="font-playfair font-semibold text-3xl mt-3">Imaginative</p>
+              </div>
+              <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+            </div>
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore reprehenderit magni vitae sunt pariatur dicta libero consequuntur voluptatum dolorem, deserunt ipsum minima illo, iusto odio! Mollitia debitis repellendus illo nisi.
+            </p>
+          </motion.div>
+        </div>
+      {/* </div> */}
     </section>
   )
 }
