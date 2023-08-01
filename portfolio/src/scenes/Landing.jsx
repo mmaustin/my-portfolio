bf0093894414b28c9db4af500fae7b3a35c6b7fa
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-01 08:21:16 -0400

    troubleshooting to fix error with contact me visual presentation

--- portfolio/src/scenes/Landing.jsx
+++ portfolio/src/scenes/Landing.jsx
@@ -77,6 +77,14 @@ const Landing = ({setSelectedPage}) => {
               Contact Me
             </AnchorLink> */}
             <AnchorLink
+              className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+                hover:bg-blue hover:text-white transition duration-500"
+              onClick={() => setSelectedPage("contact")}
+              href='#contact'
+            >
+              Contact Me
+            </AnchorLink>            
+            <AnchorLink
               className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
               onClick={()=> setSelectedPage('contact')}
               href='#contact'
