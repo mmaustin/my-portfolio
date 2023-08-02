71ce1abaaffb50cbbff26e3a0e549053acaa2ea2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-02 17:27:06 -0400

    troubleshooting project visibility issues

--- portfolio/src/scenes/Projects.jsx
+++ portfolio/src/scenes/Projects.jsx
@@ -14,7 +14,8 @@ const projectVariant = {
 }
 
 const Project = ({title}) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-red`;
 
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
@@ -78,9 +79,9 @@ const Projects = () => {
             <Project title="Project 2" />
             <Project title="Project 3" />
             <Project title="Project 4" />
-            <Project title="Project 5" />
+            {/* <Project title="Project 5" />
             <Project title="Project 6" />
-            <Project title="Project 7" />
+            <Project title="Project 7" /> */}
           <div
             className="flex justify-center text-center items-center p-10 bg-blue max-w-[400px] text-2xl font-playlist font-semibold"
           >
