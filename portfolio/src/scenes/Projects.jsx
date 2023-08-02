97a2580b484f6b777cc856908c2be1106113e4fb
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-02 10:08:39 -0400

    completed projects heading section

--- portfolio/src/scenes/Projects.jsx
+++ portfolio/src/scenes/Projects.jsx
@@ -1,8 +1,34 @@
+import { motion } from "framer-motion";
+import LineGradient from "../components/LineGradient";
 
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section id="projects" className="pt-48 pb-48">
+      <motion.div
+        className="md:w-2/4 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{once:true, amount:0.5}}
+        transition={{duration: 0.5}}
+        variants={{
+          hidden: {opacity: 0, y: -50},
+          visible: { opacity: 1, y: 0}
+        }}        
+      >
+        <div>
+          <p className="font-playlist font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">  
+            <LineGradient width="w-1/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet neque sed facilis? Sunt, dolores. A aliquid eaque, eos totam magni sed nam soluta quo nisi quis inventore quasi magnam voluptas?
+        </p>
+      </motion.div>      
+    </section>
   )
 }
 export default Projects
