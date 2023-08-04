1d247f67fa1de4821a7b5f0db6d982632438f5f8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-08-04 16:29:03 -0400

    commented out form fields and replaed with email address

--- portfolio/src/scenes/Contact.jsx
+++ portfolio/src/scenes/Contact.jsx
@@ -29,15 +29,18 @@ const Contact = () => {
       >
         <div>  
           <p className="font-playlist font-semibold text-4xl">
-            <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+            <span className="text-yellow">CONTACT ME</span> on Linkedin or via email:
           </p>
+          <p className="font-playlist font-semibold text-4xl text-yellow">
+            mccrayaustin@ymail.com
+          </p>          
           <div className="flex md:justify-end my-5">
             <LineGradient width="mx-auto w-2/4" />
           </div>
         </div>
       </motion.div>   
 
-      <div className="md:flex md:justify-between gap-16 mt-5">
+{/*      <div className="md:flex md:justify-between gap-16 mt-5">
         <motion.div
           className="basis-1/2 flex justify-center"
           initial="hidden"
@@ -66,7 +69,7 @@ const Contact = () => {
           <form
            target="_blank"
            onSubmit={onSubmit}
-           action=""
+           action="https://formsubmit.co/mccrayaustin@ymail.com"
            method="POST" 
           >
            <input
@@ -127,7 +130,7 @@ const Contact = () => {
           </form>
         </motion.div>
 
-      </div>
+      </div> */}
     </section>
   )
 }
